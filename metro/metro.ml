90f72c57536d14ce00e47381a6bacce9db5bad76
uabyss <liberalist0449@gmail.com>
2023-10-03 22:24:24 +0900

    feat: solve 13

--- metro/metro.ml
+++ metro/metro.ml
@@ -790,7 +790,6 @@ let test2 =
     - temae_list は始点の駅名のみからなるリスト
 *)
 (* shokika : eki_t list -> string -> eki_t list *)
-
 let rec shokika eki_lst kiten =
   match eki_lst with
   | [] -> []
@@ -818,3 +817,80 @@ let rec seiretsu ekimei_list =
   match ekimei_list with
   | [] -> []
   | first :: rest -> ekimei_insert (seiretsu rest) first
+
+(* 目的：直前に確定した駅 p (eki_t 型)と未確定の駅 q (eki_t 型)を受け取り、
+    p と q が直接つながっているかどうかを調べる。
+    - つながっていたら q の最短距離と手前リストを必要に応じて更新したものを返す
+    - つながっていなかったらもとの q をそのまま帰す
+*)
+(* koushin1 : eki_t -> eki_t -> eki_t *)
+let koushin1 p q =
+  match (p, q) with
+  | ( { namae = pn; saitan_kyori = ps; temae_list = pt },
+      { namae = qn; saitan_kyori = qs; temae_list = qt } ) ->
+      let kyori = get_ekikan_kyori pn qn global_ekikan_list in
+      if kyori = infinity then q
+      else if ps +. kyori < qs then
+        { namae = qn; saitan_kyori = ps +. kyori; temae_list = qn :: pt }
+      else q
+
+(* tests *)
+let eki1 = { namae = "池袋"; saitan_kyori = infinity; temae_list = [] }
+
+let eki2 = { namae = "新大塚"; saitan_kyori = 1.2; temae_list = [ "新大塚"; "茗荷谷" ] }
+
+let eki3 = { namae = "茗荷谷"; saitan_kyori = 0.; temae_list = [ "茗荷谷" ] }
+
+let eki4 = { namae = "後楽園"; saitan_kyori = infinity; temae_list = [] }
+let test1 = koushin1 eki3 eki1 = eki1
+let test2 = koushin1 eki3 eki2 = eki2
+let test3 = koushin1 eki3 eki3 = eki3
+
+let test4 =
+  koushin1 eki3 eki4
+  = { namae = "後楽園"; saitan_kyori = 1.8; temae_list = [ "後楽園"; "茗荷谷" ] }
+
+let test5 =
+  koushin1 eki2 eki1
+  = { namae = "池袋"; saitan_kyori = 3.0; temae_list = [ "池袋"; "新大塚"; "茗荷谷" ] }
+
+let test6 = koushin1 eki2 eki2 = eki2
+let test7 = koushin1 eki2 eki3 = eki3
+let test8 = koushin1 eki2 eki4 = eki4
+
+(* 直前に確定した駅 p （eki_t 型）と未確定の駅のリスト v （eki_t list 型）を受け取り、
+    必要な更新処理を行ったあと未確定の駅のリストを返す関数
+*)
+(* koushin : eki_t -> eki_t list -> eki_t list *)
+(* let rec koushin p lst =
+    match lst with
+    | [] -> []
+    | first :: rest -> koushin1 p first :: koushin p rest *)
+
+let koushin p v =
+  let f q = koushin1 p q in
+  List.map f v
+
+(* 駅の例 *)
+let eki1 = { namae = "池袋"; saitan_kyori = infinity; temae_list = [] }
+
+let eki2 = { namae = "新大塚"; saitan_kyori = 1.2; temae_list = [ "新大塚"; "茗荷谷" ] }
+
+let eki3 = { namae = "茗荷谷"; saitan_kyori = 0.; temae_list = [ "茗荷谷" ] }
+
+let eki4 = { namae = "後楽園"; saitan_kyori = infinity; temae_list = [] }
+
+(* 駅リストの例 *)
+let lst = [ eki1; eki2; eki3; eki4 ]
+
+(* テスト *)
+let test1 = koushin eki2 [] = []
+
+let test2 =
+  koushin eki2 lst
+  = [
+      { namae = "池袋"; saitan_kyori = 3.0; temae_list = [ "池袋"; "新大塚"; "茗荷谷" ] };
+      eki2;
+      eki3;
+      eki4;
+    ]
